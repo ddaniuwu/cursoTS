b09cc40bb298c998fd8168489a325896a36ad1ae
ddaniuwu <luis.daniel132@outlook.es>
2021-06-17 18:23:47 -0500

    functions

---
+++ funciones.js
@@ -0,0 +1,14 @@
+"use strict";
+//var saludo = "Hello world"
+var saludo = "Hola mundo";
+var numero = 10;
+var var1;
+var1 = "Variable no inicializada";
+var PI = 3.1416;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(var1);
+    console.log(PI);
+}
+saludar();
---
+++ funciones.ts
@@ -0,0 +1,16 @@
+//var saludo = "Hello world"
+
+let saludo:string = "Hola mundo";
+let numero:number = 10;
+let var1:string;
+var1 = "Variable no inicializada"
+const PI = 3.1416
+
+function saludar(){
+    console.log(saludo)
+    console.log(numero)
+    console.log(var1)
+    console.log(PI)
+}
+
+saludar()
