58eb867cd9888fb6b1c94f31bf40186cf39e2b39
ddaniuwu <luis.daniel132@outlook.es>
2021-06-17 22:55:01 -0500

    CAmbios en dist

--- Typescript-curso/src/main.ts
+++ Typescript-curso/src/main.ts
@@ -37,6 +37,39 @@ userInfo = `
     phone : ${phoneNumber}
     isPro : ${isUserPro}
 `
-
 console.log('userInfo' , userInfo);
 
+//------------------------------------//
+
+let a = 'hola'
+a = 'Holas'
+
+
+let a1 = 10 ; 
+let b:number = 20
+let c = a + b
+
+console.log(a)
+console.log(c)
+
+const num1 = 10
+const num2 = 20
+b = num1 + num2
+
+console.log(b)
+
+
+function suma(num1:number , num2: number):number {
+    return num1 + num2 
+}
+
+console.log(suma(2,3))
+
+/*type se utiliza para definir un tipo de dato para una 
+variable*/
+type dni = number;
+let dniNumber:dni = 1234
+let dniNumber2:dni = 123434
+
+console.log(typeof(dniNumber))
+console.log(typeof(dniNumber2))
